019a12fb700f00c23cd3d627b82052f1c3f27e22
Genevie Giko <gikogenevie@yahoo.com>
2022-02-05 23:06:42 -0500

    first commi

---
+++ js /index.js
@@ -0,0 +1,27 @@
+
+const inquirer = require('require')
+const fs = require("fs")
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+    type: "input",
+    message: "What is your GitHub username?",
+    name: "title",
+    }
+    {
+    type: "input"
+    message: "What is your email address?"
+    name:
+    }
+
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
